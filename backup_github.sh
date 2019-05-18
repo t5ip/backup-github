958995b2948100eb471d0ad59b7771d1681c148d
t5ip <pikaviesti.testitili@gmail.com>
2019-05-19 02:32:40 +0300

    Initial commit. All repos so far.

---
+++ backup_github.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+# Arguments: 
+# 1. Username 
+
+git clone https://$1@github.com/t5ip/backup-github
+git clone https://$1@github.com/t5ip/sicp-scheme
+git clone https://$1@github.com/t5ip/sicp-racket
+git clone https://$1@github.com/t5ip/wlandb
+git clone https://$1@github.com/t5ip/cellauto
+git clone https://$1@github.com/t5ip/vimrc
+git clone https://$1@github.com/t5ip/fmilcd
+git clone https://$1@github.com/t5ip/sdplanet
+git clone https://$1@github.com/t5ip/wifiscan
+git clone https://$1@github.com/t5ip/simpletimetrackerwebapp
+git clone https://$1@github.com/t5ip/SimpleTimeTracker
+git clone https://$1@github.com/t5ip/simpletimetrackerwebbackend
+git clone https://$1@github.com/t5ip/simpletimetrackerwebclient
+git clone https://$1@github.com/t5ip/ReactNativeFlatListTest
+git clone https://$1@github.com/t5ip/c-11test
+git clone https://$1@github.com/t5ip/xshop
+git clone https://$1@github.com/t5ip/nodejstest
+git clone https://$1@github.com/t5ip/midnightcurse
+git clone https://$1@github.com/t5ip/hello-world
+
